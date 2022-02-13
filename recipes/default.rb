57e42e1e5a9d6de184ea1feb4fc7bdfe33f29bbd
Joel Mann <jmann.creston@gmail.com>
2022-02-13 13:55:24 -0800

    Adding haproxy details - listener

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,22 @@
 # Recipe:: default
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
+apt_update
+
+haproxy_install 'package'
+
+haproxy_frontend 'http-in' do
+  bind '*:80'
+  default_backend 'servers'
+end
+
+haproxy_backend 'servers' do
+  server ['web1 192.168.10.43:80 maxconn 32']
+#   notifies :reload, "haproxy_service[haproxy]", :immediately 
+# doing this for a future test  - github issue -> check on a future version. 
+end
+
+haproxy_service 'haproxy' do
+  action %i(create enable start)
+  subscribes :reload 'template[/etc/haproxy/haproxy.cfg]', :immediately
+end
